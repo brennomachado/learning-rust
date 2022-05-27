d7bca7dbaeb152ef2b27daef53b27a21daa9d354
Brenno Machado <brenno.pm@gmail.com>
2022-05-27 11:36:56 -0300

    day 01

--- Rust-Alura/rust_alura/src/main.rs
+++ Rust-Alura/rust_alura/src/main.rs
@@ -1,6 +1,13 @@
-use std::mem;
+static GLOBAL_IMUTAVEL:u8 = 1;
+static mut GLOBAL_MUTAVEL:u8 = 2;
 
-fn main() {
+fn soma(a:i32, b:i32) -> i32
+{
+    println!("{} + {} = {}", a, b, a +b);
+    a + b
+}
+
+fn escopo() {
 
     let variavel = 300;
     println!("variavel = {}, tamanho = {} bytes", variavel, std::mem::size_of_val(&variavel));
@@ -11,6 +18,26 @@ fn main() {
     let mut booleana: bool = false;
     println!("tamanho da booleana = {}", std::mem::size_of_val(&booleana));
 
+    booleana = true;
+    println!("o valor da booleana é = {}", booleana);
+
     let letra:char = 'C';
     println!("Tamanho da letra = {}", std::mem::size_of_val(&letra));
+
+    const PI:f64 = 3.141592654;
+    println!("Constante PI = {}", PI);
+
+    println!("Global imutavel = {}", GLOBAL_IMUTAVEL);
+
+    unsafe {
+        println!("Global mutavel = {} - esse tipo de coisa não é seguro e deve ser evitado", GLOBAL_MUTAVEL);
+    }
+
+}
+
+fn main(){
+    escopo();
+
+    println!("Soma = {}", soma(2, 5));
+
 }
--- Rust-Alura/rust_alura/target/debug/deps/rust_alura-48ecebbbe089de76.d
+++ Rust-Alura/rust_alura/target/debug/deps/rust_alura-48ecebbbe089de76.d
@@ -1,5 +1,5 @@
-/home/brenno/Documents/Estudos/Alura/Rust-Alura/rust_alura/target/debug/deps/rust_alura-48ecebbbe089de76.rmeta: src/main.rs
+/home/brenno/Documents/Estudos/RUST/Rust-Alura/rust_alura/target/debug/deps/rust_alura-48ecebbbe089de76.rmeta: src/main.rs
 
-/home/brenno/Documents/Estudos/Alura/Rust-Alura/rust_alura/target/debug/deps/rust_alura-48ecebbbe089de76.d: src/main.rs
+/home/brenno/Documents/Estudos/RUST/Rust-Alura/rust_alura/target/debug/deps/rust_alura-48ecebbbe089de76.d: src/main.rs
 
 src/main.rs:
